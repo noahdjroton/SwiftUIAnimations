c8b28dd1afc29da9863cfcfc6f4625874b6523d8
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2024-02-13 23:47:40 +0100

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Djroton Noé SOSSOU on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
