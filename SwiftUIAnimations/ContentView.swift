f612b57bd69b41300325e65f81248427102e8c08
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2024-02-14 20:33:54 +0100

    Customizing animations in SwiftUI

--- SwiftUIAnimations/ContentView.swift
+++ SwiftUIAnimations/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Tap Me") {
+                //animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
+            )
+            .onAppear{
+                animationAmount = 2
+            }
+            
+            
         }
-        .padding()
     }
 }
 
